b11cba68ce545a4de6b4bbb9be83e9622fc163d2
Johanna <johanna.stoicanescu@gmail.com>
2022-12-02 13:21:40 +0100

    matchs page style

--- src/pages/Matchs.jsx
+++ src/pages/Matchs.jsx
@@ -1,6 +1,8 @@
 import MatchsCityContext from "../context/MatchsCity";
 import { React, useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/match.css";
+import { TbArrowBack } from "react-icons/tb";
 
 function Matchs() {
   const { cityMatched } = useContext(MatchsCityContext);
@@ -9,17 +11,43 @@ function Matchs() {
     console.log(cityMatched);
   }, []);
   return (
-    <>
-      {cityMatched.length &&
-        cityMatched.map((el) => (
-          <div>
-            <img alt="salut" src={el[0].photos[0].image.mobile}></img>
-            <p>{el[1].data.name}</p>
-          </div>
-        ))}
-
-      <Link to={`/Swap`}>Salut</Link>
-    </>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div
+        style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
+      >
+        {cityMatched.length &&
+          cityMatched.map((el) => (
+            <div
+              style={{ display: "flex", alignItems: "center", margin: "30px" }}
+            >
+              <img
+                style={{ height: "400px", borderRadius: "10px" }}
+                alt="salut"
+                src={el[0].photos[0].image.mobile}
+              ></img>
+              <p
+                style={{
+                  backgroundColor: " #2f8876",
+                  padding: "30px 40px",
+                  borderTopRightRadius: "10px",
+                  borderBottomRightRadius: "10px",
+                  color: "white",
+                  fontWeight: "600",
+                  fontSize: "20px",
+                }}
+              >
+                {el[1].data.name}
+              </p>
+            </div>
+          ))}
+      </div>
+      <Link to={`/Swap`} className="goBackButton">
+        {" "}
+        <TbArrowBack />
+      </Link>
+    </div>
   );
 }
 
